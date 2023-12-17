341cedd98f21a26c249891c96683053eaca04c77
akilesh1706 <126157319+akilesh1706@users.noreply.github.com>
2023-12-17 10:29:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,55 @@
-require("dotenv").config();
-const multer = require("multer");
-const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const File = require("./models/file");
-const express=  require("express");
-const req = require("express/lib/request");
-const app = express();
-app.use(express.urlencoded({ extended: true}));
+require("dotenv").config()
+const multer = require("multer")
+const mongoose = require("mongoose")
+const bcrypt = require("bcrypt")
+const File = require("./models/File")
 
-const upload = multer({dest: "uploads"});
+const express = require("express")
+const app = express()
+app.use(express.urlencoded({ extended: true }))
 
-mongoose.connect(process.env.DATABASE_URL);
+const upload = multer({ dest: "uploads" })
 
-app.set("view engine", "ejs");
+mongoose.connect(process.env.DATABASE_URL)
 
-app.get("/",(req,res) => {
-    res.render("index")
+app.set("view engine", "ejs")
+
+app.get("/", (req, res) => {
+  res.render("index")
 })
 
-app.post("/upload",upload.single("file"), async (req,res) => {
-    const fileData = {
-        path: req.file.path,
-        originalName: req.file.originalname
-    }
-    if(req.body.password != null && req.body.password !== ""){
-        fileData.password = await bcrypt.hash(req.body.password, 10);
-    }
-    const file = await File.create(fileData);
-    
-    res.render("index",{fileLink: `${req.headers.origin}/file/${file.id}`})
+app.post("/upload", upload.single("file"), async (req, res) => {
+  const fileData = {
+    path: req.file.path,
+    originalName: req.file.originalname,
+  }
+  if (req.body.password != null && req.body.password !== "") {
+    fileData.password = await bcrypt.hash(req.body.password, 10)
+  }
+
+  const file = await File.create(fileData)
+
+  res.render("index", { fileLink: `${req.headers.origin}/file/${file.id}` })
 })
 
-app.get("/file/:id",handleDownload);
-app.post("/file/:id",handleDownload);
+app.route("/file/:id").get(handleDownload).post(handleDownload)
 
-async function handleDownload(req,res) {
-    const file = await File.findById(req.params.id);
+async function handleDownload(req, res) {
+  const file = await File.findById(req.params.id)
 
-    if(file.password != null) {
-        if(req.body.password == null) {
-            res.render("password");
-            return;
-        }
-    }
-    //if compares whether inputted password and hashed password is matching
-    if(!(await bcrypt.compare(req.body.password, file.password))) {
-        res.render("password",{error: true});
-        return
+  if (file.password != null) {
+    if (req.body.password == null) {
+      res.render("password")
+      return
     }
 
-    await file.save();
+    if (!(await bcrypt.compare(req.body.password, file.password))) {
+      res.render("password", { error: true })
+      return
+    }
+  }
 
-    res.download(file.path,file.originalName);
+  res.download(file.path, file.originalName)
 }
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT)
